acce0eb7405c87ef9b14687e03e573aa358c44aa
Jake F - @samouri <samouri@users.noreply.github.com>
2017-06-13 13:35:36 -0400

    downgrade dashboardplugin to remove annoying debug message

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,7 @@ const CopyWebpackPlugin = require( 'copy-webpack-plugin' );
 const HardSourceWebpackPlugin = require( 'hard-source-webpack-plugin' );
 const NamedChunksPlugin = require( 'bundler/webpack-plugins/NamedChunksPlugin' );
 const NamedModulesPlugin = require( 'bundler/webpack-plugins/NamedModulesPlugin' );
+const DashboardPlugin = require( 'webpack-dashboard/plugin' );
 
 /**
  * Internal variables
@@ -91,6 +92,7 @@ const webpackConfig = {
 		fs: 'empty'
 	},
 	plugins: [
+		new DashboardPlugin(),
 		new webpack.DefinePlugin( {
 			'process.env': {
 				NODE_ENV: JSON.stringify( bundleEnv )
@@ -176,8 +178,6 @@ const jsRules = {
 };
 
 if ( calypsoEnv === 'development' ) {
-	const DashboardPlugin = require( 'webpack-dashboard/plugin' );
-	webpackConfig.plugins.splice( 0, 0, new DashboardPlugin() );
 	webpackConfig.plugins.push( new webpack.HotModuleReplacementPlugin() );
 	webpackConfig.plugins.push( new webpack.LoaderOptionsPlugin( { debug: true } ) );
 	webpackConfig.entry.build = [
